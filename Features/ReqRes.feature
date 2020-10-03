a710f42b315e47e1f348f97f82129862365c91ac
kawaljitgulati <46310041+kawaljitgulati@users.noreply.github.com>
2020-10-03 16:17:07 +0000

    Add files via upload

---
+++ Features/ReqRes.feature
@@ -0,0 +1,31 @@
+﻿Feature: Test ReqRes api
+
+Scenario: Get details of single user
+    Given ReqRes endpoint is set
+	When I set user id in request
+	And Send GET HTTP request
+	Then I receive valid HTTP response code
+
+Scenario: Get details of invalid user
+    Given ReqRes endpoint is set
+	When I set invalid user id in request
+	And Send GET HTTP request to get details
+	Then I receive invalid HTTP response code
+
+Scenario: POST request to create new user
+    Given ReqRes endpoint is set
+	When Provide new user details
+	And Send POST HTTP request
+	Then I receive valid POST HTTP response code
+
+Scenario: PUT request to create new user
+    Given ReqRes endpoint is set
+	When Provide new user details for PUT
+	And Send PUT HTTP request
+	Then I receive valid PUT HTTP response code
+
+Scenario: DELETE request to remove user
+    Given ReqRes endpoint is set
+	When Provide user id in delete endpoint
+	And Send DELETE HTTP request
+	Then I receive DELETE HTTP response code
---
+++ Steps/ReqRespSteps.cs
@@ -0,0 +1,97 @@
+﻿using NUnit.Framework;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using TechTalk.SpecFlow;
+
+namespace ReqResAutomation.Steps
+{
+    [Binding]
+    public class ReqRespSteps
+    {
+        private HttpClient _client;
+
+        private string _endpoint;
+
+        [Given(@"ReqRes endpoint is set")]
+        public void GivenTheGETReqResEndpointIsSet()
+        {
+            _endpoint = "https://reqres.in/api/users";
+
+        }
+
+        [When(@"I set user id in request")]
+        [When(@"Send GET HTTP request")]
+        [Then(@"I receive valid HTTP response code")]
+        public async Task ThenIReceiveValidHTTPResponseCode()
+        {
+            _client = new HttpClient();
+
+            _endpoint = _endpoint + "/2";
+
+            var response = await _client.GetAsync(_endpoint);
+
+            Assert.AreEqual((int)response.StatusCode, 200);
+        }
+
+        [When(@"I set invalid user id in request")]
+        [When(@"Send GET HTTP request to get details")]
+        [Then(@"I receive invalid HTTP response code")]
+        public async Task ThenIReceiveInValidHTTPResponseCode()
+        {
+            _client = new HttpClient();
+
+            _endpoint = _endpoint + "/67";
+
+            var response = await _client.GetAsync(_endpoint);
+
+            Assert.AreEqual((int)response.StatusCode, 404);
+        }
+
+
+        [When(@"Provide new user details")]
+        [When(@"Send POST HTTP request")]
+        [Then(@"I receive valid POST HTTP response code")]
+        public async Task WhenProvideNewUserDetails()
+        {
+            _client = new HttpClient();
+
+            var requestData = "{\r\n\"name\": \"morpheus\",\r\n\"job\": \"leader\"\r\n}";
+
+            var content = new StringContent(requestData, Encoding.UTF8, "application/json");
+            var response = await _client.PostAsync(_endpoint, content);
+
+            Assert.AreEqual((int)response.StatusCode, 201);
+        }
+
+        [When(@"Provide new user details for PUT")]
+        [When(@"Send PUT HTTP request")]
+        [Then(@"I receive valid PUT HTTP response code")]
+        public async Task ThenIReceiveValidPUTHTTPResponseCode()
+        {
+            _client = new HttpClient();
+
+            var requestData = "{\r\n\"name\": \"morpheus\",\r\n\"job\": \"leader\"\r\n}";
+
+            var content = new StringContent(requestData, Encoding.UTF8, "application/json");
+            var response = await _client.PostAsync(_endpoint, content);
+
+            Assert.AreEqual((int)response.StatusCode, 200);
+        }
+
+        [When(@"Provide user id in delete endpoint")]
+        [When(@"Send DELETE HTTP request")]
+        [Then(@"I receive DELETE HTTP response code")]
+        public async Task ThenIReceiveDELETEHTTPResponseCode()
+        {
+            _client = new HttpClient();
+
+            _endpoint = _endpoint + "/12";
+
+            var response = await _client.DeleteAsync(_endpoint);
+
+            Assert.AreEqual((int)response.StatusCode, 204);
+        }
+
+    }
+}
